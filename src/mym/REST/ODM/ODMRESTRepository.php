4786d6c2e02deeba1f207c3c5c54400a001eef31
Mikhail Yurasov <me@yurasov.me>
2014-03-03 07:03:00 -0800

    ODMRESTRepository
    MongoDB ODM REST Repository

---
+++ src/mym/REST/ODM/ODMRESTRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * MongoDB ODM REST Repository
+ * @copyright 2014 Mikhail Yurasov <me@yurasov.me>
+ */
+
+namespace mym\REST\ODM;
+
+use Doctrine\ODM\MongoDB\DocumentRepository;
+use mym\REST\RESTRepositoryInterface;
+
+class ODMRESTRepository extends DocumentRepository implements RESTRepositoryInterface
+{
+  protected $skip = 0;
+  protected $limit = 100;
+
+  public function searchAll()
+  {
+    return $this->createQueryBuilder()
+      ->skip($this->skip)
+      ->limit($this->limit)
+      ->getQuery()
+      ->execute();
+  }
+
+  public function searchByIds($ids = array())
+  {
+    return $this->createQueryBuilder()
+      ->limit($this->limit)
+      ->skip($this->skip)
+      ->field('id')->in($ids)
+      ->getQuery()->execute();
+  }
+
+  public function setLimit($limit)
+  {
+    $this->limit = $limit;
+  }
+
+  public function setSkip($skip)
+  {
+    $this->skip = $skip;
+  }
+}
