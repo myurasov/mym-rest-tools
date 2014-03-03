70570a8e9c918ac9840d8a39963a5c8f253821a6
Mikhail Yurasov <me@yurasov.me>
2014-03-03 07:05:11 -0800

    RESTRepositoryInterface

--- src/mym/REST/RESTRepositoryInterface.php
+++ src/mym/REST/RESTRepositoryInterface.php
@@ -7,9 +7,33 @@
 
 namespace mym\REST;
 
-interface RESTRepositoryInterface
+use Doctrine\Common\Persistence\ObjectRepository;
+
+interface RESTRepositoryInterface extends ObjectRepository
 {
+  /**
+   * @param $skip int
+   */
   public function setSkip($skip);
+
+  /**
+   * @param $limit int
+   */
   public function setLimit($limit);
+
+  /**
+   * Get all resources
+   * Limit/skip values are used
+   *
+   * @return array
+   */
   public function searchAll();
+
+  /**
+   * Search by list od IDs
+   *
+   * @param array $ids
+   * @return array
+   */
+  public function searchByIds($ids = array());
 }
