8ca284fc40857d96f1df0d28a2336b7e945c49a2
Mikhail Yurasov <me@yurasov.me>
2014-03-03 07:03:28 -0800

    ODMRESTController
    REST Controller for Doctrine MongoDB ODM

---
+++ src/mym/REST/ODM/ODMRESTController.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * REST Controller for Doctrine MongoDB ODM
+ * @copyright 2014 Mikhail Yurasov <me@yurasov.me>
+ */
+
+namespace mym\REST\ODM;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use mym\REST\RESTController;
+
+class ODMRESTController extends RESTController
+{
+  /**
+   * @var DocumentManager
+   */
+  protected $dm;
+
+  /**
+   * @var ODMRESTRepository
+   */
+  protected $repository;
+
+  public function setOm($om)
+  {
+    $this->dm = $this->om = $om;
+  }
+
+  protected function deleteCollection()
+  {
+    $this->repository->createQueryBuilder()
+      ->remove()
+      ->getQuery()
+      ->execute();
+  }
+}
